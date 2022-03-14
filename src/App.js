c1972c8e5b4a2a4429e370dc033d50c0e2b0e01c
Jetom88 <shinhro3o@gmail.com>
2022-03-14 14:49:03 +0900

    useEffect 기능 살펴보기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+
+  console.log("여러번!");
+
+  useEffect(() => {
+    console.log("한번만");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("search", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>Hello jetom</h1>
-      <Button text={"continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }

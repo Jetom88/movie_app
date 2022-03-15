0c30727f6dbe8ff82b7751fb5c5206c64029d27d
Jetom88 <shinhro3o@gmail.com>
2022-03-15 18:52:59 +0900

    map 연습 및 비트코인 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  const [btcChange, setBtcChange] = useState(0);
+  const onChange = (e) => {
+    setBtcChange(e.target.value);
   };
+
   return (
     <div>
-      <h1>My To Dos: {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="write"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>Coins {loading ? "" : `${coins.length}💸`}</h1>
+      {loading ? (
+        <strong>Loading..</strong>
+      ) : (
+        <>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <form>
+            <label htmlFor="btc">
+              얼만큼의 BTC를 살 수 있을까요?{" "}
+              {btcChange !== 0
+                ? `${btcChange / coins[0].quotes.USD.price} BTC`
+                : "🤔"}
+            </label>
+            <br />
+            <input
+              onChange={onChange}
+              value={btcChange}
+              id="btc"
+              type="number"
+            />
+          </form>
+        </>
+      )}
     </div>
   );
 }
